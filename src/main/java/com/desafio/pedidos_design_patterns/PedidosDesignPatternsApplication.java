3cfab3590bb0d742ecc269ac1762711748cb37ba
Anderson <andersonmessias827@gmail.com>
2026-08-12 22:43:54 -0300

    feat: implementacao da API de Pedidos utilizando padroes GoF (Strategy, Facade e Singleton)

---
+++ src/main/java/com/desafio/pedidos_design_patterns/PedidosDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.pedidos_design_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/pedidos_design_patterns/PedidosDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pedidos_design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
